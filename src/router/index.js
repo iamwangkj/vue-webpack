8359f73f6ea46ee50c540fbe95d934544a71878b
wangkj <wangkaijie5@jd.com>
2020-03-26 18:36:11 +0800

    add page android-css

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,11 @@ export const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/android-css',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import('../pages/About.vue')
+    component: () => import('../pages/android-css.vue')
   },
   {
     path: '/font-size',
@@ -24,7 +23,7 @@ export const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import('../pages/FontSize.vue')
+    component: () => import('../pages/font-size.vue')
   }
 ]
 
