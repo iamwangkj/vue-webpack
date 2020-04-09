f394a58c9c73d66bf5676b40ca4161a4dbff7147
wangkj <wangkaijie5@jd.com>
2020-04-09 18:27:54 +0800

    css show differently according to phone theme

--- src/router/index.js
+++ src/router/index.js
@@ -11,11 +11,11 @@ export const routes = [
     component: Home
   },
   {
-    path: '/android-css',
+    path: '/theme-day-and-night',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import('../pages/android-css.vue')
+    component: () => import('../pages/theme-day-and-night.vue')
   },
   {
     path: '/font-size',
